05066404fce900befca5e6ff7466bbb23d76e067
Konrad Reiche <konrad.reiche@fokus.fraunhofer.de>
2013-03-11 13:58:50 +0100

    Make translator configuration file path sensitive
    I forgot to make the configuration parsing in the translator module path
    sensitive as well. Before the translator code for mapping the groups
    could not find the configuration file.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@fokus.fraunhofer.de>

--- ckanext/govdatade/harvesters/translator.py
+++ ckanext/govdatade/harvesters/translator.py
@@ -1,12 +1,13 @@
 #!/bin/env python
 import ConfigParser
 import json
+import os
 import urllib2
 
 
 def translate_groups(groups, source_name):
     config = ConfigParser.RawConfigParser()
-    config.read('config.ini')
+    config.read(os.path.dirname(os.path.abspath(__file__)) + '/config.ini')
     url = config.get('URLs', 'categories') + source_name + '2deutschland.json'
     json_string = urllib2.urlopen(url).read()
     group_dict = json.loads(json_string)
@@ -14,5 +15,5 @@ def translate_groups(groups, source_name):
     result = []
     for group in groups:
         if group in group_dict:
-            result.append(group_dict[group])
+            result = result + group_dict[group]
     return result
