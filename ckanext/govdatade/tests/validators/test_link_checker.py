e7d72bb35db687e87d339143903ffbf11f210b9e
Konrad Reiche <konrad.reiche@fokus.fraunhofer.de>
2013-12-16 10:14:03 +0100

    Write methods for URL validation and dataset check
    Just added more tests to cover the functionality, especially special
    cases like follow location.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@fokus.fraunhofer.de>

--- ckanext/govdatade/tests/validators/test_link_checker.py
+++ ckanext/govdatade/tests/validators/test_link_checker.py
@@ -8,12 +8,50 @@ class TestLinkChecker:
     def setUp(self):
         self.link_checker = LinkChecker()
 
-    def test_is_available(self):
-        response = [200, "OK"]
-        assert self.link_checker.is_available(response)
+    def test_is_available_200(self):
+        assert self.link_checker.is_available(200)
+
+    def test_is_available_404(self):
+        assert not self.link_checker.is_available(400)
 
     @httpretty.activate
-    def test_check_url(self):
-        url = "http://example.com/dataset/1"
-        httpretty.register_uri(httpretty.GET, url, status=200)
-        self.link_checker.is_available([200, "OK"])
+    def test_check_url_200(self):
+        url = 'http://example.com/dataset/1'
+        httpretty.register_uri(httpretty.HEAD, url, status=200)
+
+        expectation = 200
+        assert self.link_checker.validate(url) == expectation
+
+    @httpretty.activate
+    def test_check_url_404(self):
+        url = 'http://example.com/dataset/1'
+        httpretty.register_uri(httpretty.HEAD, url, status=404)
+
+        expectation = 404
+        assert self.link_checker.validate(url) == expectation
+
+    @httpretty.activate
+    def test_check_url_301(self):
+        url = 'http://example.com/dataset/1'
+        target = 'http://www.example.com/dataset/1'
+
+        httpretty.register_uri(httpretty.HEAD, target, status=200)
+        httpretty.register_uri(httpretty.HEAD, url, status=301,
+                               location=target)
+
+        expectation = 200
+        assert self.link_checker.validate(url) == expectation
+
+    @httpretty.activate
+    def test_check_dataset(self):
+        url1 = 'http://example.com/dataset/1'
+        httpretty.register_uri(httpretty.HEAD, url1, status=200)
+        url2 = 'http://example.com/dataset/2'
+        httpretty.register_uri(httpretty.HEAD, url2, status=404)
+        url3 = 'http://example.com/dataset/3'
+        httpretty.register_uri(httpretty.HEAD, url3, status=200)
+
+        dataset = {'id': 1,
+                   'resources': [{'url': url1}, {'url': url2}, {'url': url3}]}
+
+        assert self.link_checker.check_dataset(dataset) == [200, 404, 200]
--- ckanext/govdatade/validators/link_checker.py
+++ ckanext/govdatade/validators/link_checker.py
@@ -1,7 +1,19 @@
-class LinkChecker:
-    def __init__(self):
-        pass
+import requests
 
-    def is_available(self, response):
-        response_code = response[0]
+
+class LinkChecker:
+
+    HEADERS = {'User-Agent': 'curl/7.29.0'}
+
+    def check_dataset(self, dataset):
+        results = []
+        for resource in dataset['resources']:
+            results.append(self.validate(resource['url']))
+        return results
+
+    def validate(self, url):
+        response = requests.head(url, allow_redirects=True)
+        return response.status_code
+
+    def is_available(self, response_code):
         return response_code >= 200 and response_code < 300
